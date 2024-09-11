4901b395be78eb871961e57335948cb1027664a2
Sohee Kim <ttohee1275@gmail.com>
2024-09-12 08:37:20 +0900

    FEAT :: design system

---
+++ project/src/style/theme.jsx
@@ -0,0 +1,28 @@
+export const theme = {
+  color: {
+    black: "#2F2F2F",
+    white: "#FFFFFF",
+
+    main: {
+      1: "#9AEE6E",
+      2: "#84E153",
+      3: "#6AD433",
+      4: "#50C314",
+      5: "#39A600",
+    },
+    gray: {
+      1: "#EBEBEB",
+      2: "#DBDBDB",
+      3: "#CCCCCC",
+      4: "#B9B9B9",
+      5: "#AAAAAA",
+      6: "#9E9E9E",
+      7: "#959595",
+      8: "#8B8B8B",
+    },
+
+    error: {
+      1: "F9291C",
+    },
+  },
+};
