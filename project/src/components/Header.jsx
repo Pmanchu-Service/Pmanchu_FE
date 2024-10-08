359f4e320d145361f4c2d9794b01e22e256f0ec1
kimsiwoo <ksw98035213@gmail.com>
2024-10-08 16:41:16 +0900

    FEAT :: Header추가

---
+++ project/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const Header = () => {
+  return(
+    <Conrainer>
+
+    </Conrainer>
+  )
+}
+
+const Conrainer = styled.div`
+  width: 1920px;
+  height: 64px;
+  background-color: red;
+`
