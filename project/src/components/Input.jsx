2785c1c22c6cb7210e66c3f590c5882a07905baf
Sohee Kim <ttohee1275@gmail.com>
2024-10-10 08:38:46 +0900

    FEAT :: input 태그 수정

--- project/src/components/Input.jsx
+++ project/src/components/Input.jsx
@@ -10,7 +10,7 @@ export const Input = (props) => {
         <TitleContainer>
           <Text>{props.text}</Text>
         </TitleContainer>
-        <StyledInput></StyledInput>
+        <StyledInput />
       </InputContainer>
     </>
   );
