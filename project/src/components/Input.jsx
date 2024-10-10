a176e21864f507ea2c7966f52f02968545ba9ee9
Sohee Kim <ttohee1275@gmail.com>
2024-10-10 22:11:24 +0900

    FEAT :: input component

--- project/src/components/Input.jsx
+++ project/src/components/Input.jsx
@@ -3,24 +3,28 @@ import styled from "styled-components";
 import { theme } from "../style/theme";
 import star from "../assets/mingcute_asterisk-fill.svg";
 
-export const Input = (props) => {
+export const Input = ({ height = "100%", ...props }) => {
   return (
     <>
       <InputContainer>
         <TitleContainer>
           <Text>{props.text}</Text>
+          <StyledImg src={star} />
         </TitleContainer>
-        <StyledInput />
+        <StyledInput placeholder={props.placeholder} height={height} />
       </InputContainer>
     </>
   );
 };
 
+const StyledImg = styled.img``;
 const TitleContainer = styled.div`
   display: flex;
   justify-content: flex-start;
-  float: left;
-  background: url(${star});
+  align-items: flex-start;
+  width: 87px;
+  height: 28px;
+  margin-bottom: 8px;
 `;
 const Text = styled.p`
   font-family: "regular";
@@ -30,10 +34,18 @@ const Text = styled.p`
 const InputContainer = styled.div`
   display: flex;
   justify-content: flex-start;
+  flex-direction: column;
 `;
 const StyledInput = styled.input`
   width: 690px;
-  height: 40px;
+  height: ${({ height }) => height};
+  border-radius: 8px;
   background-color: ${theme.color.gray[1]};
   border: 1px solid ${theme.color.gray[2]};
+  padding-left: 12px;
+  ::placeholder {
+    font-size: 16px;
+    font-weight: 400;
+    color: ${theme.color.gray[5]};
+  }
 `;
