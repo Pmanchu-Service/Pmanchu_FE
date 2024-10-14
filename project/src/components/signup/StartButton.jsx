9d26556424d6ffc907276bc7294f11ee923d5772
Sohee Kim <ttohee1275@gmail.com>
2024-10-14 23:07:52 +0900

    PERF :: 버튼 수정

--- project/src/components/signup/StartButton.jsx
+++ project/src/components/signup/StartButton.jsx
@@ -4,27 +4,21 @@ import styled from "styled-components";
 export const StartButton = () => {
   return (
     <>
-      <StyledDiv>
-        <StyledButton>시작하기</StyledButton>
-      </StyledDiv>
+      <StyledButton>시작하기</StyledButton>
     </>
   );
 };
 
-const StyledDiv = styled.div`
-  width: 690px;
-  height: 60px;
-  border-radius: 10px;
-  border: none;
-  background-color: ${theme.color.gray[4]};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 const StyledButton = styled.button`
   border: none;
   color: ${theme.color.white};
   font-size: 32px;
   font-weight: 600;
-  background: none;
+  width: 690px;
+  border-radius: 10px;
+  background-color: ${theme.color.gray[4]};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 11px 0;
 `;
