6153a3a388534cb63131dbe8e6fe3f0981ecd535
김 찬 <gimchan29@naver.com>
2024-10-16 08:29:53 +0900

    FEAT :: ProjectForm 페이지 퍼블리싱 완료

--- project/src/pages/ProjectFormPage.jsx
+++ project/src/pages/ProjectFormPage.jsx
@@ -15,33 +15,64 @@ export const ProjectForm = () => {
     { id: '1011', name: '양병건', role: '예 저는 이 프로젝트에서 귀여움을 담당하고 있습니다' }
   ];
 
+  // DataTable의 customStyles 정의
+  const customStyles = {
+    rows: {
+      style: {
+        borderBottom: '1px solid #ccc', // 요소 사이에 선 추가
+      },
+    },
+    headCells: {
+      style: {
+        fontWeight: '700',
+        backgroundColor: '#f0f0f0', // 헤더 배경색 추가
+      },
+    },
+  };
+
   return (
     <>
       <Header />
       <Container>
         <Title>프로젝트 보고서</Title>
         <StyledHr />
+      <Project>
         <SubTitle>프로젝트</SubTitle>
         <StyledHr />
         <Setting>프로젝트 이름</Setting>
-        <Edit>프만추</Edit>
+          <Edit>프만추</Edit>
         <StyledHr />
         <Setting>팀명</Setting>
-        <Edit>프만추</Edit>
+          <Edit>프만추</Edit>
         <StyledHr />
         <Setting>팀원 및 역할</Setting>
         <DataTableContainer>
           <DataTable
             columns={columns}
             data={data}
-            noHeader
+            customStyles={customStyles} // customStyles 속성 추가
+            noHeader={false} // 헤더를 표시
             pagination={false}
           />
         </DataTableContainer>
         <StyledHr />
+      </Project>
+      <Outline>
+        <SubTitle>개요</SubTitle>
+        <StyledHr/>
+        <Setting>프로젝트 동기</Setting>
+          <Edit>제 동기요? 집에가고싶은 간절한 그 마음이 제 유일한 동기입니다 ㅅㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ</Edit>
+        <StyledHr/>
+        <Setting>주요 서비스</Setting>
+          <Edit>제 동기요? 집에가고싶은 간절한 그 마음이 제 유일한 동기입니다 ㅅㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ</Edit>
+        <StyledHr/>
+        <Setting>상세기능</Setting>
+          <Edit>제 동기요? 집에가고싶은 간절한 그 마음이 제 유일한 동기입니다 ㅅㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ</Edit>
+        <StyledHr/>
+      </Outline>
       </Container>
     </>
-  )
+  );
 };
 
 const Container = styled.div`
@@ -65,8 +96,21 @@ const SubTitle = styled.h1`
 
 const StyledHr = styled.hr`
   clear: both;
-  margin-top: 10px;
+  margin-top: 25px;
+  &:last-child
+  {
+    margin-bottom: 100px;
+  }
 `;
+
+const Project = styled.div`
+  margin: 0; 
+`
+
+const Outline = styled.div`
+  float: left;
+  text-align: left;
+`
 
 const Setting = styled.span`
   float: left;
@@ -86,6 +130,9 @@ const Edit = styled.span`
 `;
 
 const DataTableContainer = styled.div`
+  width: 95%;
+  margin: 0 auto;
   margin-top: 20px;
+  margin-bottom: 30px;
   text-align: left;
 `;
