e8dfd1adef977d1dffbf64fcdb58633e9058e1fa
김 찬 <gimchan29@naver.com>
2024-10-15 23:01:55 +0900

    FEAT :: ProjectFrom 페이지 퍼블리싱

--- project/src/Router.jsx
+++ project/src/Router.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Mypage } from './pages/Mypage';
 import { UserSearchPage } from './pages/UserSearchPage';
 import { CheckPostPage } from './pages/CheckPostPage';
-import { PlanPage } from './pages/PlanInquiryPage';
 import { MainPage } from './pages/MainPage';
 import { HeadHuntingPage } from './pages/HeadHuntingPage';
 import { Signup } from './pages/Signup';
+import { ProjectForm } from './pages/ProjectFormPage';
 
 // import { Mypage_me } from './pages/Mypage_me';
 
@@ -13,14 +13,14 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/mypage_me" element={<Mypage_me />} /> */}
+        {/* <Route path="/mypage_me" element={<M  ypage_me />} /> */}
         <Route path="/" element={<MainPage />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/usersearch" element={<UserSearchPage />} />
         <Route path="/check" element={<CheckPostPage />} />
         <Route path="/HeadHuntingPage" element={<HeadHuntingPage />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/plan" element={<PlanPage />} />
+        <Route path="/plan" element={<ProjectForm />} />
       </Routes>
     </BrowserRouter>
   );
