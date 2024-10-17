7895b325fe5dcd17350c6a662791be3150671099
김 찬 <gimchan29@naver.com>
2024-10-17 20:29:25 +0900

    FEAT :: CheckPostPage퍼블리싱

--- project/src/pages/CheckPostPage.jsx
+++ project/src/pages/CheckPostPage.jsx
@@ -1,13 +1,12 @@
 import { Header } from '../components/Header';
 import { PostUser } from '../components/checkpost/PostUser';
-import { TagComponent } from '../components/checkpost/TagComponent';
-
 
 export const CheckPostPage = () => {
   return (
     <>
     <Header />
     <PostUser />
+
     </>
   );
 }
